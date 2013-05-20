a10f2ad898a6bd909079230d84b706dc6b63ba8d
Bing Liu <liubinging@gmail.com>
2013-05-20 15:07:37 +0800

    Add item to provide content for resultList page. Add resultList view.

--- src/main/java/cn/edu/ustb/model/BookListItemModel.java
+++ src/main/java/cn/edu/ustb/model/BookListItemModel.java
@@ -1,9 +1,16 @@
 package cn.edu.ustb.model;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class BookListItemModel {
 	private String imageSrc;
 	private String title;
 	private String author;
+	private String publisher;
+	private String date;
+	private String price;
 	private int star;
 	
 	public String getImageSrc() {
@@ -14,6 +21,15 @@ public class BookListItemModel {
 	}
 	public String getAuthor() {
 		return author;
+	}
+	public String getPublisher() {
+		return publisher;
+	}
+	public String getDate() {
+		return date;
+	}
+	public String getPrice() {
+		return price;
 	}
 	public int getStar() {
 		return star;
@@ -27,10 +43,24 @@ public class BookListItemModel {
 	public void setAuthor(String author) {
 		this.author = author;
 	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	public void setDate(Date date) {
+		DateFormat df = new SimpleDateFormat("yyyy-MM");
+		this.date = df.format(date);
+	}
+	public void setPrice(int price) {
+		this.price = String.format("%.2f", price/100.0);
+	}
 	public void setStar(int star) {
 		this.star = star;
 	}
 	public void setStar(int star1, int star2, int star3, int star4, int star5) {
-		this.star = (star1 + 2*star2 + 3*star3 + 4*star4 + 5*star5) / (star1+star2+star3+star4+star5);
+		int total = star1+star2+star3+star4+star5;
+		if(total != 0)
+			this.star = (star1 + 2*star2 + 3*star3 + 4*star4 + 5*star5) / (star1+star2+star3+star4+star5);
+		else
+			this.star = 0;
 	}
 }
---
+++ src/main/webapp/WEB-INF/views/resultList.jsp
@@ -0,0 +1,7 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ include file="includes/header.jsp"%>
+
+
+
+<%@ include file="includes/footer.jsp"%>
